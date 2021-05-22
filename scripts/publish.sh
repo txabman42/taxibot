b0480c4a468a0f0db112981cf82123e8c8f4b6d8
Xabier <xabier.martinez@cabify.com>
2021-05-22 09:52:44 +0200

    Modify repository from dockerhub to jfrog

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,14 +3,16 @@
 set -eufo pipefail
 
 release="off"
+jfrog_server="txabman.jfrog.io"
+repository_name="default-docker-local"
 
 # check required commands and env variables are in place
 command -v docker >/dev/null 2>&1 || { echo 'please install docker or use image that has it'; exit 1; }
-if [[ -z "${DOCKER_USERNAME}" ]]; then
-    echo 'DOCKER_USERNAME env variable is empty'; exit 1;
+if [[ -z "${JFROG_USERNAME}" ]]; then
+    echo 'JFROG_USERNAME env variable is empty'; exit 1;
 fi
-if [[ -z "${DOCKER_PASSWORD}" ]]; then
-    echo 'DOCKER_PASSWORD env variable is empty'; exit 1;
+if [[ -z "${JFROG_PASSWORD}" ]]; then
+    echo 'JFROG_PASSWORD env variable is empty'; exit 1;
 fi
 
 while getopts "t:" opt; do
@@ -22,7 +24,6 @@ while getopts "t:" opt; do
   esac
 done
 
-docker_hub_prefix="xmartinezb"
 image="taxibot"
 tag="latest"
 
@@ -30,11 +31,12 @@ if [[ $release == "on" ]]; then
     tag=$(git describe --abbrev=0 --tags)   
 fi
 
-image_hub=$docker_hub_prefix/$image:$tag
+image_hub=$jfrog_server/$repository_name/$image:$tag
+
+echo "IMAGE HUB $image_hub"
 
 # push to DockerHub
-echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
+echo ${JFROG_PASSWORD} | docker login $jfrog_server -u ${JFROG_USERNAME} --password-stdin
 docker tag $image $image_hub
 docker push $image_hub
 docker rmi $image_hub
-docker logout
